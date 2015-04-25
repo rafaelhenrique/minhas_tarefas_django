5fc6fdc6052abf41ae995411de77be138a498a25
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2015-04-25 11:13:22 -0300

    Hide fields on form

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -9,5 +9,8 @@ class TarefaAdmin(ModelAdmin):
     # valores que vc vai conseguir acessar como um atributo
     list_display = ['nome', 'descricao', 'data_de_criacao',
                     'data_de_execucao', 'finalizado']
+    # exclude = ['data_de_execucao']
+    # exibir somente esses campos no form
+    fields = ['nome', 'descricao', 'finalizado']
 
 site.register(Tarefa, TarefaAdmin)
