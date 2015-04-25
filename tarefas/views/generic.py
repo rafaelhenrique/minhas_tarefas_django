3193384453f1c20f15f23f96da9f4e6fe35142e1
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2015-04-25 11:58:50 -0300

    Adding generic view to detail information of row

--- tarefas/views/generic.py
+++ tarefas/views/generic.py
@@ -1,4 +1,5 @@
 from django.views.generic import ArchiveIndexView
+from django.views.generic import DetailView
 
 from tarefas.models import Tarefa
 
@@ -6,6 +7,10 @@ from tarefas.models import Tarefa
 class TarefasView(ArchiveIndexView):
     model = Tarefa
     date_field = 'data_de_criacao'
+
+
+class TarefaDetail(DetailView):
+    model = Tarefa
 
 # Cria uma lista com nome das coisas que vc quer disponibilizar
 __all__ = [
