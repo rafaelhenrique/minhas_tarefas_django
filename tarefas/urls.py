2b5f72fd6c055509291c0f567923e0bbab7dccf5
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2015-04-25 14:32:03 -0300

    Adding url with named paramether for /tarefas/id

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls import patterns, url
 
 from tarefas.views import Home
 from tarefas.views import TarefasView
+from tarefas.views import TarefaDetail
 
 urlpatterns = patterns(
     '',
     # as_view envelopa a class dentro de uma funcao
     url(r'^$', Home.as_view(), name='home'),
     url(r'^tarefas/$', TarefasView.as_view(), name='tarefas'),
+    url(r'^tarefas/(?P<pk>\d+)/$',
+        TarefaDetail.as_view(),
+        name='tarefas_detail'),
 )
