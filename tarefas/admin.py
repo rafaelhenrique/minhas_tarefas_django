5ef4885288e9de5864b4ef4d1adcb23dd06f37be
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2015-04-25 11:16:06 -0300

    Adding multiple select for finalizado field on general list

--- tarefas/admin.py
+++ tarefas/admin.py
@@ -9,8 +9,13 @@ class TarefaAdmin(ModelAdmin):
     # valores que vc vai conseguir acessar como um atributo
     list_display = ['nome', 'descricao', 'data_de_criacao',
                     'data_de_execucao', 'finalizado']
+    # nao mostrar esse campo
     # exclude = ['data_de_execucao']
+
     # exibir somente esses campos no form
     fields = ['nome', 'descricao', 'finalizado']
+
+    # quais campos serao editaveis na lista geral
+    list_editable = ['finalizado']
 
 site.register(Tarefa, TarefaAdmin)
