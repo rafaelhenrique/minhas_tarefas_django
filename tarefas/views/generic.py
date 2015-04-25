2f17b180f73392805de1486ed920dca407240730
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2015-04-25 11:54:54 -0300

    Adding generic view to show rows ordered by date

--- tarefas/views/generic.py
+++ tarefas/views/generic.py
@@ -5,6 +5,7 @@ from tarefas.models import Tarefa
 
 class TarefasView(ArchiveIndexView):
     model = Tarefa
+    date_field = 'data_de_criacao'
 
 # Cria uma lista com nome das coisas que vc quer disponibilizar
 __all__ = [
