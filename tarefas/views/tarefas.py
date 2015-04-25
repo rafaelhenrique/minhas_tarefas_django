18812dddf43bc304aba0ff0ecb8b6c8d854b94c7
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2015-04-25 15:08:03 -0300

    Adding feature 'tarefas abertas' on the home page

--- tarefas/views/tarefas.py
+++ tarefas/views/tarefas.py
@@ -1,11 +1,12 @@
 # view mais basica usada para criar as nossas views
 from django.views.generic import View
-
 # Converter o template para que o browser consiga renderizar
 from django.shortcuts import render_to_response
 # Usado pra nao perder dado de context e talz
 # transferencia de uma requisicao pra outra
 from django.template.context import RequestContext
+
+from tarefas.models import Tarefa
 
 
 class Home(View):
@@ -13,6 +14,10 @@ class Home(View):
     context = {}
 
     def get(self, request, *args, **kwargs):
+        # Este objects eh a instancia de um manager que executa as funcoes
+        # de acesso ao banco
+        self.context['counter'] = Tarefa.objects.filter(
+            finalizado=False).count()
         return render_to_response(self.template_name,
                                   self.context, RequestContext(request))
 
